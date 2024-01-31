9df8c421e45849aa2ec11c056c0498167164525f
ccwnc <140484169+ccwnc@users.noreply.github.com>
2024-01-31 18:21:05 +0900

    Feat: Add AuthContext, withAuth, withAuthForLanding

--- src/components/commons/Buttons/SignButton.tsx
+++ src/components/commons/Buttons/SignButton.tsx
@@ -8,11 +8,12 @@ interface SignButtonProps {
   children: React.ReactNode;
   type?: "button" | "submit" | "reset" | undefined;
   isDisabled?: boolean;
+  onClick: () => void;
 }
 
-export const SignButton = ({ children, type = "button", isDisabled = false }: SignButtonProps) => {
+export const SignButton = ({ children, type = "button", isDisabled = false, onClick }: SignButtonProps) => {
   return (
-    <button className={cx("btn", { disabled: isDisabled })} disabled={isDisabled} type={type}>
+    <button className={cx("btn", { disabled: isDisabled })} disabled={isDisabled} type={type} onClick={onClick}>
       <span>{children}</span>
     </button>
   );
