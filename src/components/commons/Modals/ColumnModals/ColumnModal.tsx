b7d4d49daba8cf9ca132ba371d276c43231b6a65
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-02-02 17:29:43 +0900

    feat: add Modal Background

--- src/components/commons/Modals/ColumnModals/ColumnModal.tsx
+++ src/components/commons/Modals/ColumnModals/ColumnModal.tsx
@@ -5,21 +5,22 @@ import classNames from "classnames/bind";
 import ResponseBtn from "../../Buttons/ResponseButton";
 import { useState } from "react";
 import NiceModal, { useModal } from "@ebay/nice-modal-react";
+import ModalBackground from "../ModalBackground";
 
 const cx = classNames.bind(styles);
 
 interface Props {
   isEdit?: boolean;
-  onCancle: () => void;
+  onCancel: () => void;
 }
 
 export default NiceModal.create(({ isEdit = false }: Props) => {
   const modal = useModal();
 
-  return <ColumnModal isEdit={isEdit} onCancle={modal.remove} />;
+  return <ColumnModal isEdit={isEdit} onCancel={modal.remove} />;
 });
 
-function ColumnModal({ isEdit, onCancle }: Props) {
+function ColumnModal({ isEdit, onCancel }: Props) {
   const [isSureDelete, setIsSureDelete] = useState(false);
   const { control, handleSubmit, formState } = useForm({ mode: "onChange" });
 
@@ -32,44 +33,50 @@ function ColumnModal({ isEdit, onCancle }: Props) {
   };
 
   return !isSureDelete ? (
-    <article className={cx("modal-container")}>
-      <h2 className={cx("title")}>{isEdit ? "칼럼 관리" : "칼럼 생성"}</h2>
-      <form className={cx("form")} onSubmit={handleSubmit(handleOnSubmit)}>
-        <Input
-          isModal={true}
-          type="text"
-          name="columnName"
-          labelName="이름"
-          placeholder="새로운 프로젝트"
-          control={control}
-          rules={{ required: "프로젝트 이름을 입력해 주세요" }}
-        />
+    <>
+      <article className={cx("modal-container")}>
+        <h2 className={cx("title")}>{isEdit ? "칼럼 관리" : "칼럼 생성"}</h2>
+        <form className={cx("form")} onSubmit={handleSubmit(handleOnSubmit)}>
+          <Input
+            isModal={true}
+            type="text"
+            name="columnName"
+            labelName="이름"
+            placeholder="새로운 프로젝트"
+            control={control}
+            rules={{ required: "프로젝트 이름을 입력해 주세요" }}
+          />
+          <div className={cx("btn-line")}>
+            <ResponseBtn onClick={onCancel} state="cancel" ph={1.4} fs={1.6}>
+              취소
+            </ResponseBtn>
+            <ResponseBtn type="submit" disabled={!formState.isValid} state="accept" ph={1.4} fs={1.6}>
+              {isEdit ? "변경" : "생성"}
+            </ResponseBtn>
+          </div>
+        </form>
+        {isEdit && (
+          <button type="button" onClick={handelonClickDelete} className={cx("delete-column-btn")}>
+            삭제하기
+          </button>
+        )}
+      </article>
+      <ModalBackground onClick={onCancel} />
+    </>
+  ) : (
+    <>
+      <article className={cx("modal-container")}>
+        <h2 className={cx("delete-message")}>칼럼의 모든 카드가 삭제됩니다</h2>
         <div className={cx("btn-line")}>
-          <ResponseBtn onClick={onCancle} state="cancel" ph={1.4} fs={1.6}>
+          <ResponseBtn onClick={handelonClickDelete} state="cancel" ph={1.4} fs={1.6}>
             취소
           </ResponseBtn>
-          <ResponseBtn type="submit" disabled={!formState.isValid} state="accept" ph={1.4} fs={1.6}>
-            {isEdit ? "변경" : "생성"}
+          <ResponseBtn type="submit" state="accept" ph={1.4} fs={1.6}>
+            삭제
           </ResponseBtn>
         </div>
-      </form>
-      {isEdit && (
-        <button type="button" onClick={handelonClickDelete} className={cx("delete-column-btn")}>
-          삭제하기
-        </button>
-      )}
-    </article>
-  ) : (
-    <article className={cx("modal-container")}>
-      <h2 className={cx("delete-message")}>칼럼의 모든 카드가 삭제됩니다</h2>
-      <div className={cx("btn-line")}>
-        <ResponseBtn onClick={handelonClickDelete} state="cancel" ph={1.4} fs={1.6}>
-          취소
-        </ResponseBtn>
-        <ResponseBtn type="submit" state="accept" ph={1.4} fs={1.6}>
-          삭제
-        </ResponseBtn>
-      </div>
-    </article>
+      </article>
+      <ModalBackground onClick={onCancel} />
+    </>
   );
 }
--- src/components/commons/Modals/TaskModals/TaskModal.tsx
+++ src/components/commons/Modals/TaskModals/TaskModal.tsx
@@ -8,23 +8,24 @@ import { ChangeEvent, useState } from "react";
 import Dropdown from "./DropdownForTaskModals/DropdownForTaskModals";
 import NiceModal, { NiceModalHandler, useModal } from "@ebay/nice-modal-react";
 import DateInput from "../../Input/DateInput";
+import ModalBackground from "../ModalBackground";
 
 const cx = classNames.bind(styles);
 
 interface Props {
   isEdit?: boolean;
-  onCancle?: () => void;
+  onCancel: () => void;
 }
 
 export default NiceModal.create(({ isEdit = false }: Props) => {
   const modal = useModal();
 
-  return <TaskModal isEdit={isEdit} onCancle={modal.remove} />;
+  return <TaskModal isEdit={isEdit} onCancel={modal.remove} />;
 });
 
 //상태 값 dashboardId 로 변환해서 submit
 
-function TaskModal({ isEdit = false, onCancle }: Props) {
+function TaskModal({ isEdit = false, onCancel }: Props) {
   const [startDate, setStartDate] = useState(new Date());
 
   const { control, setValue, handleSubmit, formState } = useForm({ mode: "onBlur" });
@@ -34,44 +35,54 @@ function TaskModal({ isEdit = false, onCancle }: Props) {
     console.log(data);
   }
   return (
-    <article className={cx("modal-container")}>
-      <h2 className={cx("title")}>{isEdit ? "할 일 수정" : "할 일 생성"}</h2>
-      <form className={cx("form")} onSubmit={handleSubmit(handleOnSubmit)}>
-        <div className={cx("dropdown-line")}>
-          {isEdit && <Dropdown name="dashboardId" setValue={setValue} control={control} />}
-          <Dropdown name="assignee" setValue={setValue} control={control} />
-        </div>
+    <>
+      <article className={cx("modal-container")}>
+        <h2 className={cx("title")}>{isEdit ? "할 일 수정" : "할 일 생성"}</h2>
+        <form className={cx("form")} onSubmit={handleSubmit(handleOnSubmit)}>
+          <div className={cx("dropdown-line")}>
+            {isEdit && <Dropdown name="dashboardId" setValue={setValue} control={control} />}
+            <Dropdown name="assignee" setValue={setValue} control={control} />
+          </div>
 
-        <Input
-          isModal={true}
-          type="text"
-          name="title"
-          labelName="제목"
-          placeholder="제목을 입력해 주세요"
-          control={control}
-          rules={{ required: "제목을 입력해 주세요" }}
-        />
-        <Textarea
-          isModal={true}
-          name="description"
-          labelName="설명"
-          placeholder="설명을 입력해 주세요"
-          control={control}
-          rules={{ required: "설명을 입력해 주세요" }}
-        />
-        <DateInput labelName="마감일" control={control} name="dueDate" />
+          <Input
+            isModal={true}
+            type="text"
+            name="title"
+            labelName="제목"
+            placeholder="제목을 입력해 주세요"
+            control={control}
+            rules={{ required: "제목을 입력해 주세요" }}
+          />
+          <Textarea
+            isModal={true}
+            name="description"
+            labelName="설명"
+            placeholder="설명을 입력해 주세요"
+            control={control}
+            rules={{ required: "설명을 입력해 주세요" }}
+          />
+          <DateInput labelName="마감일" control={control} name="dueDate" />
 
-        <Input isModal={true} type="text" name="tag" labelName="태그" placeholder="입력 후 Enter" control={control} />
-        <Input isModal={true} type="file" name="profileImageUrl" labelName="이미지" placeholder="" control={control} />
-        <div className={cx("btn-line", { edit: isEdit })}>
-          <ResponseBtn type="button" onClick={onCancle} state="cancel" ph={1.4} fs={1.6}>
-            취소
-          </ResponseBtn>
-          <ResponseBtn type="submit" disabled={!formState.isValid} state="accept" ph={1.4} fs={1.6}>
-            {isEdit ? "수정" : "생성"}
-          </ResponseBtn>
-        </div>
-      </form>
-    </article>
+          <Input isModal={true} type="text" name="tag" labelName="태그" placeholder="입력 후 Enter" control={control} />
+          <Input
+            isModal={true}
+            type="file"
+            name="profileImageUrl"
+            labelName="이미지"
+            placeholder=""
+            control={control}
+          />
+          <div className={cx("btn-line", { edit: isEdit })}>
+            <ResponseBtn type="button" onClick={onCancel} state="cancel" ph={1.4} fs={1.6}>
+              취소
+            </ResponseBtn>
+            <ResponseBtn type="submit" disabled={!formState.isValid} state="accept" ph={1.4} fs={1.6}>
+              {isEdit ? "수정" : "생성"}
+            </ResponseBtn>
+          </div>
+        </form>
+      </article>
+      <ModalBackground onClick={onCancel} />
+    </>
   );
 }
