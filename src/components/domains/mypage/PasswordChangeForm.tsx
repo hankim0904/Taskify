28b4d9d83b3291aa6788486cf1bcf2be6649984a
handsup <dhfma3394@naver.com>
2024-02-05 19:50:46 +0900

    Feat: Add password change function

--- src/components/domains/mypage/PasswordChangeForm.tsx
+++ src/components/domains/mypage/PasswordChangeForm.tsx
@@ -5,12 +5,18 @@ import styles from "./PasswordChangeForm.module.scss";
 import classNames from "classnames/bind";
 import putChangePassword from "@/api/putChangePassword";
 import { useAuth } from "@/contexts/AuthContext";
+import { useEffect } from "react";
 
 const cx = classNames.bind(styles);
 
 export default function PasswordChangeForm() {
   const { accessToken } = useAuth();
-  const { control, handleSubmit } = useForm({ mode: "onChange" });
+  const {
+    control,
+    watch,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm({ mode: "onChange" });
 
   const onSubmit: SubmitHandler<FieldValues> = data => {
     putChangePassword(accessToken, data.currentPassword, data.newPassword);
@@ -39,7 +45,10 @@ export default function PasswordChangeForm() {
                 labelName="새 비밀번호"
                 control={control}
                 placeholder="새 비밀번호 입력"
-                rules={{ required: "새 비밀번호를 입력해 주세요" }}
+                rules={{
+                  required: "새 비밀번호를 입력해 주세요",
+                  minLength: { value: 8, message: "8자 이상 입력해 주세요." },
+                }}
               />
             </div>
             <div className={cx("contents-input-area-password")}>
@@ -49,13 +58,16 @@ export default function PasswordChangeForm() {
                 labelName="새 비밀번호 확인"
                 control={control}
                 placeholder="새 비밀번호 확인"
-                rules={{ required: "비밀번호 확인을 위해 한번 더 입력해 주세요" }}
+                rules={{
+                  required: "비밀번호 확인을 위해 한번 더 입력해 주세요",
+                  validate: value => (value === watch("newPassword") ? true : "비밀번호가 일치하지 않습니다."),
+                }}
               />
             </div>
           </div>
         </div>
         <div className={cx("contents-btn")}>
-          <ResponseBtn state="accept" type="submit" ph={0.8}>
+          <ResponseBtn state="accept" type="submit" disabled={!isValid} ph={0.8}>
             변경
           </ResponseBtn>
         </div>
