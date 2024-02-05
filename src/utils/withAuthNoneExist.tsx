5cafb2dff955840970db70ccd9086aaf08658e13
ccwnc <ccwnc337@gmail.com>
2024-02-05 21:15:47 +0900

    Fix: Modify logic to get accessToken in withAuthNoneExist

--- src/utils/withAuthNoneExist.tsx
+++ src/utils/withAuthNoneExist.tsx
@@ -1,17 +1,20 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { useAuth } from "@/contexts/AuthContext";
 
 const withAuthNoneExist = <P extends {}>(WrappedComponent: React.ComponentType<P>) => {
   const AuthenticatedComponent = (props: P) => {
     const router = useRouter();
-    const { accessToken } = useAuth();
 
     useEffect(() => {
+      const accessToken = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("accessToken="))
+        ?.split("=")[1];
+
       if (!accessToken) {
         router.push("signin");
       }
-    }, [accessToken]);
+    }, []);
 
     return <WrappedComponent {...props} />;
   };
