98bdc77360287d6b18828c143d792dede4762e8a
ccwnc <ccwnc337@gmail.com>
2024-02-08 15:19:37 +0900

    Chore: update file

--- src/api/getUsersMe.ts
+++ src/api/getUsersMe.ts
@@ -1,10 +1,6 @@
 import { axiosCSRInstance } from "./axiosCSRInstance";
 
-<<<<<<< HEAD
-export default async function getUsersMe(accessToken: string) {
-=======
 export default async function getUsersMe(accessToken: string | null) {
->>>>>>> 31cb24977735484b0cede8ee41eeae8585bb1514
   try {
     const res = await axiosCSRInstance.get("users/me", {
       headers: {
