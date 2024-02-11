b8f0fa572d9d882c7dfe0e10cc6166a68184b853
woohyun kim <rladngus133@naver.com>
2024-02-11 16:37:39 +0900

    fix: SSR issue fixed (#80)

--- src/components/commons/Dashboard/Dashboard.tsx
+++ src/components/commons/Dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import styles from "./Dashboard.module.scss";
 import classNames from "classnames/bind";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const cx = classNames.bind(styles);
 
@@ -15,12 +16,12 @@ export interface DashboardProp {
 
 export default function Dashboard({ color, isHost = false, isSidebar = false, children }: DashboardProp) {
   return (
-    <div className={cx("dashboard", { sidebar: isSidebar })}>
+    <motion.div className={cx("dashboard", { sidebar: isSidebar })} whileHover={{ x: 10 }}>
       <span className={cx("dashboard-dot")} style={{ backgroundColor: color }}></span>
       <span className={cx("dashboard-title", { sidebar: isSidebar })}>{children}</span>
       <span className={cx("dashboard-icon", { sidebar: isSidebar })}>
         {isHost && <Image fill src="/assets/icons/ic-crown.svg" alt="왕관 모양 아이콘" />}
       </span>
-    </div>
+    </motion.div>
   );
 }
